b229f42ec29455341f57e3423afc6a7382c2e92c
jingxc <jingxiaochong@HUAWEI-MacBook-Pro.local>
2024-04-29 11:49:59 +0800

    https

--- src/components/wechat/scan.vue
+++ src/components/wechat/scan.vue
@@ -1,23 +1,61 @@
 <template>
-    <!--手机联系人组件-->
-    <div>
+  <div>
+    <button @click="toggleFullScreen">cccccccc</button>
     <video ref="video" autoplay></video>
-    <canvas ref="canvas" style="display: none;"></canvas>
-        123
-    </div>
+    <button @click="startCamera">Start Camera</button>
+    <button @click="stopCamera">Stop Camera</button>
+  </div>
 </template>
+
 <script>
 export default {
-    data() {
-        return {}
+  methods: {
+    toggleFullScreen() {
+      if (
+        document.fullscreenEnabled ||
+        document.mozFullScreenEnabled ||
+        document.webkitFullscreenEnabled ||
+        document.msFullscreenEnabled
+      ) {
+        console.log('浏览器支持全屏功能')
+      } else {
+        console.log('浏览器不支持全屏功能')
+      }
+      const element = document.getElementById('app')
+      console.log(element)
+      if (element.requestFullscreen) {
+        console.log(element.requestFullscreen)
+        // return
+        element.requestFullscreen()
+      } else if (element.mozRequestFullScreen) {
+        // Firefox
+        element.mozRequestFullScreen()
+      } else if (element.webkitRequestFullscreen) {
+        // Chrome, Safari and Opera
+        element.webkitRequestFullscreen()
+      } else if (element.msRequestFullscreen) {
+        // Internet Explorer and Edge
+        element.msRequestFullscreen()
+      }
     },
-    created() { },
-    // const video = this.$refs.video;
-    // const canvas = this.$refs.canvas;
-    // const context = canvas.getContext("2d");
-
-    // canvas.width = video.videoWidth;
-    // canvas.height = video.videoHeight;
-    // context.drawImage(video, 0, 0, canvas.width, canvas.height);
-}
+    async startCamera() {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        this.$refs.video.srcObject = stream;
+      } catch (error) {
+        console.error('Error accessing camera:', error);
+      }
+    },
+    stopCamera() {
+      const stream = this.$refs.video.srcObject;
+      const tracks = stream.getTracks();
+      tracks.forEach(track => track.stop());
+      this.$refs.video.srcObject = null;
+    }
+  }
+};
 </script>
+
+<style scoped>
+/* Add styles as needed */
+</style>
