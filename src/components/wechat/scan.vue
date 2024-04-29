db109953dfd801ee82db5befb26b2c7bb597052f
jingxc <jingxiaochong@HUAWEI-MacBook-Pro.local>
2024-04-29 18:04:21 +0800

    自动播放

--- src/components/wechat/scan.vue
+++ src/components/wechat/scan.vue
@@ -1,65 +1,55 @@
 <template>
   <div>
-    <img src="/images/scans.png" style="width: 100%;">
-    <!-- <button @click="toggleFullScreen">cccccccc</button>
-    <video ref="video" autoplay></video>
+    <!-- <img src="/images/scans.png" style="width: 100%;"> -->
+    <video src="./测试视频.mp4" autoplay controls ref="gga" v-if="play"></video>
+    <video ref="video" autoplay controls></video>
     <button @click="startCamera">Start Camera</button>
-    <button @click="stopCamera">Stop Camera</button> -->
+    <button @click="stopCamera">Stop Camera</button>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      play: false,
+    }
+  },
   methods: {
-    toggleFullScreen() {
-      if (
-        document.fullscreenEnabled ||
-        document.mozFullScreenEnabled ||
-        document.webkitFullscreenEnabled ||
-        document.msFullscreenEnabled
-      ) {
-        console.log('浏览器支持全屏功能')
-      } else {
-        console.log('浏览器不支持全屏功能')
-      }
-      const element = document.getElementById('app')
-      console.log(element)
-      if (element.requestFullscreen) {
-        console.log(element.requestFullscreen)
-        // return
-        element.requestFullscreen()
-      } else if (element.mozRequestFullScreen) {
-        // Firefox
-        element.mozRequestFullScreen()
-      } else if (element.webkitRequestFullscreen) {
-        // Chrome, Safari and Opera
-        element.webkitRequestFullscreen()
-      } else if (element.msRequestFullscreen) {
-        // Internet Explorer and Edge
-        element.msRequestFullscreen()
-      }
-    },
     async startCamera() {
+      this.play = true
+      setTimeout(() => {
+        this.$refs.gga.play()
+      }, 1000);
+      return
       try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-        this.$refs.video.srcObject = stream;
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: { width: 100, height: 100, facingMode: 'environment' },
+        })
+        // const stream = await navigator.mediaDevices.getUserMedia({
+        //   video: true,
+        // })
+        console.log(stream);
+        alert(stream.active)
+        alert(stream.id)
+        this.$refs.video.srcObject = stream
       } catch (error) {
-        console.error('Error accessing camera:', error);
+        console.error('Error accessing camera:', error)
       }
     },
     stopCamera() {
-      const stream = this.$refs.video.srcObject;
-      const tracks = stream.getTracks();
-      tracks.forEach(track => track.stop());
-      this.$refs.video.srcObject = null;
-    }
+      const stream = this.$refs.video.srcObject
+      const tracks = stream.getTracks()
+      tracks.forEach((track) => track.stop())
+      this.$refs.video.srcObject = null
+    },
   },
   mounted() {
     setTimeout(() => {
-      window.location.href = 'https://6437cab4291ee50001318391.caiyicloud.com/reserve/66221da421954b000139ed43'
-    }, 1000);
-  }
-};
+      // window.location.href = 'https://6437cab4291ee50001318391.caiyicloud.com/reserve/66221da421954b000139ed43'
+    }, 1000)
+  },
+}
 </script>
 
 <style scoped>
